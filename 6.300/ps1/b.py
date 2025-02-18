8afa6bb2e2653a86a9aa5b7a2d690eee01ca9955
Miguel Flores-Acton <mfacton1@gmail.com>
2025-02-18 02:30:07 -0500

    Save

--- 6.300/ps1/b.py
+++ 6.300/ps1/b.py
@@ -1,20 +1,38 @@
-from math import sin, pi
+from math import pi, sin
+
 from matplotlib.pyplot import plot, show
 
-x = []
-y = []
 
-def f(t):
+def plot_func(func):
+    x = []
+    y = []
+
+    t = 0
+    while t<10:
+        x.append(t)
+        y.append(func(t))
+        t += 0.01
+
+    plot(x,y)
+
+def approximate(t):
     sum = 0
     for k in range(2, 10000, 2):
         sum += sin(k*t)/k
     return sum
 
-t = 0
-while t<10:
-    x.append(t)
-    y.append(f(t))
-    t += 0.01
+def top_bound(t):
+    return pi/4
 
-plot(x,y)
+def bottom_bound(t):
+    return -pi/4
+
+def closed_form(t):
+    return -t/2 % (pi/2) - pi/4
+        
+plot_func(approximate)
+# plot_func(top_bound)
+# plot_func(bottom_bound)
+plot_func(closed_form)
+
 show()
--- 6.300/ps1/c.py
+++ 6.300/ps1/c.py
@@ -1,20 +1,38 @@
-from math import sin, pi
+from math import floor, pi, sin
+
 from matplotlib.pyplot import plot, show
 
-x = []
-y = []
 
-def f(t):
+def plot_func(func):
+    x = []
+    y = []
+
+    t = 0
+    while t<10:
+        x.append(t)
+        y.append(func(t))
+        t += 0.01
+
+    plot(x,y)
+
+def approximate(t):
     sum = 0
     for k in range(1, 10000, 2):
         sum += sin(k*t)/k
     return sum
 
-t = 0
-while t<10:
-    x.append(t)
-    y.append(f(t))
-    t += 0.01
+def top_bound(t):
+    return pi/4
 
-plot(x,y)
+def bottom_bound(t):
+    return -pi/4
+
+def closed_form(t):
+    return pi/4*(-1)**floor(t/pi)
+        
+plot_func(approximate)
+# plot_func(top_bound)
+# plot_func(bottom_bound)
+plot_func(closed_form)
+
 show()
